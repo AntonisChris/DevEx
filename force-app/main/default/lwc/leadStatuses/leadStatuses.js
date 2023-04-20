eb6b37b0c28a858b338c454710e09ef0a35011e4
Antonis Christoforakis <ant.christoforakis@gmail.com>
2023-04-20 09:48:22 +0300

    fix search

--- force-app/main/default/lwc/leadStatuses/leadStatuses.js
+++ force-app/main/default/lwc/leadStatuses/leadStatuses.js
@@ -4,6 +4,13 @@ import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 export default class LeadStatuses extends LightningElement {
   leads;
+  allleads;
+  timer;
+
+  columns = [
+    { label: "Name", fieldName: "Name" },
+    { label: "Status", fieldName: "Status" }
+  ];
 
   @wire(getAllLeadsAndStatuses)
   handleLeads({ error, data }) {
@@ -13,6 +20,7 @@ export default class LeadStatuses extends LightningElement {
         Name: lead.Name,
         Status: lead.Lead_Statuses__r[0].Status__c
       }));
+      this.allleads = [...this.leads];
       console.log(data);
     } else if (error) {
       console.log(error);
@@ -26,8 +34,18 @@ export default class LeadStatuses extends LightningElement {
     }
   }
 
-  columns = [
-    { label: "Name", fieldName: "Name" },
-    { label: "Status", fieldName: "Status" }
-  ];
+  handleSearchChange(event) {
+    this.debounceSearch(event.target.value);
+  }
+
+  handleSearch = (searchKey) => {
+    this.leads = this.allleads.filter((lead) => {
+      return lead.Name.toLowerCase().includes(searchKey.toLowerCase());
+    });
+  };
+
+  debounceSearch(searchKey, delay = 500) {
+    if (this.timer) clearTimeout(this.timer);
+    this.timer = setTimeout(() => this.handleSearch(searchKey), delay);
+  }
 }
